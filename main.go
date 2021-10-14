ccdf9e34f50cc27ab353eaed892ae52ada9adec1
nickherrig <neherrig@gmail.com>
2021-10-13 19:35:49 -0500

    Decode resp.Body to struct instead of map[string]interface{}

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"os"
 	"encoding/json"
 	"fmt"
-	"time"
 	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-// API url endpoint for Hair Jordan's appointments 
+// API url endpoint for Hair Jordan's appointments
 var url string = "https://www.genbook.com/bookings/api/serviceproviders/30230662/services/989056738/resources/989056742"
 
+type body struct {
+	Timezone     map[string]interface{} `json:"timezone"`
+	Dates        []string               `json:"dates"`
+	Bookingdates []string               `json:"bookingdates`
+}
 
 func main() {
 
@@ -23,36 +28,32 @@ func main() {
 		logger.Fatal(err)
 	}
 
-	var data map[string]interface{}
-
-	err = json.NewDecoder(resp.Body).Decode(&data)
+	var bd body
+	err = json.NewDecoder(resp.Body).Decode(&bd)
 	if err != nil {
 		logger.Fatal(err)
 	}
 
 	var lstminapts []time.Time
-	for _, apt := range data["bookingdates"].([]interface{}){
+	for _, apt := range bd.Bookingdates {
 
-		aptstr := apt.(string)
-
-		year :=  aptstr[:4]
-		month := aptstr[4:6]
-		day := aptstr[6:8]
+		year := apt[:4]
+		month := apt[4:6]
+		day := apt[6:8]
 
 		dts := fmt.Sprintf("%s-%s-%sT00:00:00.000Z", year, month, day)
 
-                t, err := time.Parse(time.RFC3339, dts)
-	        if err != nil {
+		t, err := time.Parse(time.RFC3339, dts)
+		if err != nil {
 			logger.Fatal(err)
-	        }
+		}
 
 		now := time.Now()
 		dif := t.Sub(now)
 
-		if dif < 40 * 24 * time.Hour {
+		if dif < 30*24*time.Hour {
 			lstminapts = append(lstminapts, t)
 		}
-
 
 	}
 
@@ -64,18 +65,18 @@ func main() {
 	}
 
 	type Data struct {
-		Barber string
+		Barber       string
 		Appointments []time.Time
 	}
 
 	d := Data{"Jordan", lstminapts}
 	tmpl := template.Must(template.ParseFiles("templates/index.html.tmpl"))
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-            tmpl.Execute(w, d)
-        })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, d)
+	})
 
-        fs := http.FileServer(http.Dir("./static"))
-        http.Handle("/static/", http.StripPrefix("/static", fs))
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 
 	err = srv.ListenAndServe()
 	logger.Fatal(err)
