2fe7ef4c9a1d9f0934af58c0c597d3a7ba5fb187
nickherrig <neherrig@gmail.com>
2021-10-16 09:53:33 -0500

    Facelift, Add all barbers data

--- main.go
+++ main.go
@@ -8,53 +8,77 @@ import (
 	"net/http"
 	"os"
 	"time"
+//	"sync/atomic"
 )
 
-// API url endpoint for Hair Jordan's appointments
-var url string = "https://www.genbook.com/bookings/api/serviceproviders/30230662/services/989056738/resources/989056742"
 
-type body struct {
-	Timezone     map[string]interface{} `json:"timezone"`
-	Dates        []string               `json:"dates"`
+type response struct {
+	Timezone     map[string]interface{} `json:"timezone"` // don't care about this data
+	Dates        interface{}            `json:"dates"`    // don't care about this data
 	Bookingdates []string               `json:"bookingdates`
+}
+
+type barber struct {
+	Name string
+	Img  string
+	Apts []time.Time
 }
 
 func main() {
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	resp, err := http.Get(url)
-	if err != nil {
-		logger.Fatal(err)
+	genbookapi := "https://www.genbook.com/bookings/api/serviceproviders/"
+
+	barberendpoints := map[string]string{
+		"jordan":  fmt.Sprintf("%s/30230662/services/989056738/resources/989056742", genbookapi),
+		"pete":    fmt.Sprintf("%s/31191440/services/10282291592/resources/10282190294", genbookapi),
+		"brandon": fmt.Sprintf("%s/30377943/services/2394050193/resources/2394025610", genbookapi),
+		"luis":    fmt.Sprintf("%s/30250062/services/1173749692/resources/1173749696", genbookapi),
+		"zach":    fmt.Sprintf("%s/30302725/services/1547629284/resources/1547629288", genbookapi),
+		"paul":    fmt.Sprintf("%s/30309745/services/1603733980/resources/1603733984", genbookapi),
+		"kegan":   fmt.Sprintf("%s/30352805/services/2098565278/resources/2098565282", genbookapi),
 	}
 
-	var bd body
-	err = json.NewDecoder(resp.Body).Decode(&bd)
-	if err != nil {
-		logger.Fatal(err)
-	}
+	var barbers []barber
+	for k, v := range  barberendpoints {
+		var b barber
+		b.Name = k
+		b.Img = fmt.Sprintf("/static/img/%s.jpeg", b.Name)
 
-	var lstminapts []time.Time
-	for _, apt := range bd.Bookingdates {
-
-		year := apt[:4]
-		month := apt[4:6]
-		day := apt[6:8]
-
-		dts := fmt.Sprintf("%s-%s-%sT00:00:00.000Z", year, month, day)
-
-		t, err := time.Parse(time.RFC3339, dts)
+		resp, err := http.Get(v)
 		if err != nil {
 			logger.Fatal(err)
 		}
 
-		now := time.Now()
-		dif := t.Sub(now)
-
-		if dif < 30*24*time.Hour {
-			lstminapts = append(lstminapts, t)
+		var r response
+		err = json.NewDecoder(resp.Body).Decode(&r)
+		if err != nil {
+			logger.Fatal(err)
 		}
 
+		for _, apt := range r.Bookingdates {
+
+			year := apt[:4]
+			month := apt[4:6]
+			day := apt[6:8]
+
+			dts := fmt.Sprintf("%s-%s-%sT00:00:00.000Z", year, month, day)
+
+			t, err := time.Parse(time.RFC3339, dts)
+			if err != nil {
+				logger.Fatal(err)
+			}
+
+			now := time.Now()
+			dif := t.Sub(now)
+
+			if dif < 7*24*time.Hour {
+				b.Apts = append(b.Apts, t)
+			}
+
+		}
+		barbers = append(barbers, b)
 	}
 
 	srv := &http.Server{
@@ -64,20 +88,15 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	type Data struct {
-		Barber       string
-		Appointments []time.Time
-	}
 
-	d := Data{"Jordan", lstminapts}
 	tmpl := template.Must(template.ParseFiles("templates/index.html.tmpl"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, d)
+		tmpl.Execute(w, barbers)
 	})
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 
-	err = srv.ListenAndServe()
+	err := srv.ListenAndServe()
 	logger.Fatal(err)
 }
